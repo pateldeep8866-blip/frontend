bdd4d81e78b81697101b744e4e2367c58e560054
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2026-02-23 17:17:06 -0600

    Make Sakura ink branch visibly prominent in theme background

--- src/app/page.js
+++ src/app/page.js
@@ -2661,6 +2661,10 @@ export default function Home() {
         {isCherry && (
           <div className="pointer-events-none absolute inset-0 z-[1] overflow-hidden sakura-scene sakura-scene-pro" aria-hidden="true">
             {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/sakura-ink-branch-vertical.svg" alt="" className="sakura-ink-feature sakura-ink-feature-left" />
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/sakura-ink-branch-vertical.svg" alt="" className="sakura-ink-feature sakura-ink-feature-right" />
+            {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src="/sakura-tree.svg" alt="" className="sakura-tree sakura-tree-left" />
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src="/sakura-tree.svg" alt="" className="sakura-tree sakura-tree-canopy" />
